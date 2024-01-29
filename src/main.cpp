c815957038e3c6294da606f48eff88b17c150d6b
Jaunie Williams <jauniew21@gmail.com>
2024-01-28 19:50:22 -0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+#include <Arduino.h>
+
+// #define TRIG 11
+// #define ECHO 9
+// #define LED 2
+
+// long duration;
+// float distance;
+
+void setup() {
+  // put your setup code here, to run once:
+  // pinMode(TRIG, OUTPUT);
+  // pinMode(ECHO, INPUT);
+
+  // pinMode(LED, OUTPUT);
+
+  // Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // digitalWrite(TRIG, LOW);
+  // delayMicroseconds(2);
+
+  // digitalWrite(TRIG, HIGH);
+  // delayMicroseconds(10);
+  // digitalWrite(TRIG, LOW);
+
+  // duration = pulseIn(ECHO, HIGH);
+
+  // distance = duration * 0.034 /2;
+
+  // Serial.print("Distance: ");
+  // Serial.print(distance);
+  // Serial.println(" cm");
+
+  // if (distance < 15) {
+  //   digitalWrite(LED, HIGH);
+  //   delay(300);
+  // } else {
+  //   digitalWrite(LED, LOW);
+  // }
+
+  // delay(300);
+}
